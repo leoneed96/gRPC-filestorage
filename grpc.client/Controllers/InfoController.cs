0e08cbc3dc948406d80b4a83fcbbaf3ced2e48ef
leoneed96 <coxerkm@gmail.com>
2021-01-15 22:10:21 +0300

    init. gRPC learn & research

---
+++ grpc.client/Controllers/InfoController.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static grpc.types.Animal;
+
+namespace grpc.client.Controllers
+{
+    [Route("info")]
+    public class InfoController: ControllerBase
+    {
+        [HttpGet]
+        [Route("animal")]
+        public async Task<IActionResult> GetAninalInfoFromGrpc([FromQuery]string name)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new AnimalClient(channel);
+            var reply = await client.GetDataAsync(
+                              new types.AnimalRequest { Title = name });
+
+            return Ok(reply);
+        }
+    }
+}
---
+++ grpc.service/Services/AnimalService.cs
@@ -0,0 +1,37 @@
+﻿using grpc.types;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace grpc.service.Services
+{
+    public class AnimalService: Animal.AnimalBase
+    {
+        public override async Task<AnimalResponse> GetData(AnimalRequest request, ServerCallContext context)
+        {
+            switch (request.Title.ToLower())
+            {
+                case "dog":
+                    return new AnimalResponse()
+                    {
+                        Sound = "waff",
+                        Weight = 10
+                    };
+                case "cat":
+                    return new AnimalResponse()
+                    {
+                        Sound = "mew!",
+                        Weight = 5
+                    };
+                default:
+                    return new AnimalResponse()
+                    {
+                        Sound = "unknown",
+                        Weight = 0
+                    };
+            }
+        }
+    }
+}
